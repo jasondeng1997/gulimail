b3e5d807b37c5b96b50a1eac7da0f9a49358796d
jasondeng1997 <15301580353@163.com>
2021-05-18 18:17:48 +0800

    yes

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/feign/GmallSmsClient.java
+++
@@ -1,9 +0,0 @@
-package com.atguigu.gmall.pms.feign;
-
-import com.atguigu.gmall.sms.api.GmallSmsApi;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient("sms-service")
-public interface GmallSmsClient extends GmallSmsApi {
-
-}
